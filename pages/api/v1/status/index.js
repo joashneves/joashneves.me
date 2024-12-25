b48240286a32a67de3454e531e4342f0fc874f4f
joashneves <joashneves@gmail.com>
2024-12-24 23:41:43 -0300

    adicionado o arquivo jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 async function status(request, response){
   const result = await database.query("SELECT 1 + 1 as sum;");
   console.log(result.rows);
-  response.status(200).json({ Hello_world: "Ola mundo"});
+  response.status(200).json({ Hello_world: "Ola mundo", console: `${result.rows}`});
 };
 
 export default status;
