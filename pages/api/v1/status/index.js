1df2f5b394e5ebd222442052ce181a3fb9127a9e
joashneves <joashneves@gmail.com>
2024-12-25 02:02:48 -0300

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
-async function status(request, response){
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Hello_world: "Ola mundo", console: `${result.rows}`});
-};
+async function status(request, response) {
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionDatabase = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionDatabaseValue = openedConnectionDatabase.rows[0].count;
+  const versionDatabase = await database.query("SHOW server_version;");
+  const versionDatabaseValue = versionDatabase.rows[0].server_version;
+
+  const maxConnectionsDatabase = await database.query("SHOW max_connections;");
+  const maxConnectionsDatabaseValue = parseInt(
+    maxConnectionsDatabase.rows[0].max_connections,
+  );
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionDatabaseValue,
+        max_connections: maxConnectionsDatabaseValue,
+        opened_connection: openedConnectionDatabaseValue,
+      },
+    },
+  });
+}
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET /api/v1/status deve retornar 200", async () =>{
   const response = await fetch("http://26.137.54.75:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connection).toEqual(1);
 });
