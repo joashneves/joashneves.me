1b3f4eb4e6ab6549102e15e49752ecccfbe1020e
joashneves <joashneves@gmail.com>
2024-12-24 21:39:03 -0300

    conectar usando o `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response){
-  response.status(200).json("Ola mundo");
-}
+import database from "../../../../infra/database.js"
+
+async function status(request, response){
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ Hello_world: "Ola mundo"});
+};
 
 export default status;
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Site Pessoal
+  Esse é um projeto para guardar e documentar meus outros projetos, e estou usando cocmo base o curso.dev 
