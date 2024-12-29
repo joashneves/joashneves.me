1af9ce30f73b7b04bfe061f0067d0f28ab75fe2c
joashneves <joashneves@gmail.com>
2024-12-29 16:42:19 -0300

    resolve

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
@@ -7,7 +7,7 @@ export default async function migrations(request, response) {
   const defaultMigrationOption = {
     databaseUrl: dbClient,
     dryRun: true,
-    dir: join("infra", "migration"),
+    dir: resolve("infra", "migration"),
     direction: "up",
     migrationsTable: "pgmigrations",
     verbose: true,
