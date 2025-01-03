7d78f91d5f67f351d35147dead69419cfae24fed
O Roxo <141419406+joashneves@users.noreply.github.com>
2025-01-03 12:59:47 +0000

    change version database 16.0 to 17.2

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,7 +14,7 @@ test("GET /api/v1/status deve retornar 200", async () => {
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("17.2");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connection).toEqual(1);
 });
