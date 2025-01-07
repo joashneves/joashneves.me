3badf27f2826cea39bbb1abebc4b04d5aa9e35a6
O Roxo <141419406+joashneves@users.noreply.github.com>
2025-01-07 03:47:02 +0000

    fix: fix a `/status` page

--- pages/status/index.js
+++ pages/status/index.js
@@ -2,49 +2,48 @@ import { useState } from "react";
 import useSWR from "swr";
 
 async function fetchApi(key) {
-    const response = await fetch(key);
-    const responsebody = await response.json();
-    return responsebody;
+  const response = await fetch(key);
+  const responsebody = await response.json();
+  return responsebody;
 }
 
-export default function StatusPage(){
-    
-    return(
-        <>
-        <h1>Status</h1>
-        <UpdateAt/>
-        <ShowAPIStatus/>
-        </>
-    )
+export default function StatusPage() {
+  return (
+    <>
+      <h1>Status</h1>
+      <UpdateAt />
+      <ShowAPIStatus />
+    </>
+  );
 }
 
-function UpdateAt(){
-    const {isLoading, data} = useSWR("/api/v1/status", fetchApi, {
-        refreshInterval: 2000
-    });
-    let updateAtText = "..."
-    const [nowLoading, setNowLoading] = useState(isLoading);
-    if(!isLoading && data){
-        updateAtText = new Date(data.updated_at).toLocaleString("pt-BR")
-    }
+function UpdateAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
+  let updateAtText = "...";
+  if (!isLoading && data) {
+    updateAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+  }
 
-    return <div>Ultima atualização: {updateAtText}</div>
+  return <div>Ultima atualização: {updateAtText}</div>;
 }
 
 function ShowAPIStatus() {
-    const { data, isLoading } = useSWR("/api/v1/status", fetchApi, {
-      refreshInterval: 2000,
-    });
-  
-    return isLoading ? (
-      <div>Carregando...</div>
-    ) : (
-      <>
-        <div>Versão do PostgreSQL: {data.dependencies.database.version}</div>
-        <div>Conexões máximas: {data.dependencies.database.max_connections}</div>
-        <div>
-          Conexões abertas: {data.dependencies.database.openned_connections}
-        </div>
-      </>
-    );
-  }
+  const { data, isLoading } = useSWR("/api/v1/status", fetchApi, {
+    refreshInterval: 2000,
+  });
+  console.log("carregando...");
+  return isLoading ? (
+    <div>Carregando...</div>
+  ) : (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div>Versão do PostgreSQL: {data.dependencies.database.version}</div>
+      <div>Conexões máximas: {data.dependencies.database.max_connections}</div>
+      <div>
+        Conexões abertas: {data.dependencies.database.opened_connection}
+      </div>
+    </>
+  );
+}
