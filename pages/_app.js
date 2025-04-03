15d21058bb7dfb5accd305793e68a234d8881d01
joashneves <joashneves@gmail.com>
2025-04-03 19:20:56 -0300

    build: install primerect

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,22 @@ import "./App.css";
 import Footer from "src/components/footer/Footer.jsx";
 import HudMenu from "src/components/HudMenu/HudMenu";
 import { TemplateConfigProvider } from "src/services/template/TemplateConfigContext";
+import { PrimeReactProvider } from "primereact/api";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <TemplateConfigProvider value={pageProps}>
-        <div className="hudContent">
-          <HudMenu />
-          <div className="mainContent">
-            <Component {...pageProps} />
+      <PrimeReactProvider>
+        <TemplateConfigProvider value={pageProps}>
+          <div className="hudContent">
+            <HudMenu />
+            <div className="mainContent">
+              <Component {...pageProps} />
+            </div>
           </div>
-        </div>
-        <Footer />
-      </TemplateConfigProvider>
+          <Footer />
+        </TemplateConfigProvider>
+      </PrimeReactProvider>
     </>
   );
 }
