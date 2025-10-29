ea968027e3127f8a9b28cab3ff12398cc4ab90e2
joashneves <joashneves@gmail.com>
2025-10-29 11:13:55 -0300

    feat: adiciona /portifolio a pagina

--- pages/portifolio/index.js
+++ pages/portifolio/index.js
@@ -1,10 +1,7 @@
 import Head from 'next/head';
-import styles from '../projetos/projetos.module.css'; // Adjusted path
 import fs from 'fs';
 import path from 'path';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import rehypeRaw from 'rehype-raw';
+import MarkdownRenderer from '../../src/components/MarkdownRenderer';
 
 export default function Portifolio({ content }) {
   return (
@@ -12,12 +9,9 @@ export default function Portifolio({ content }) {
       <Head>
         <title>Portifólio</title>
       </Head>
-      <section className={styles.container}>
-        <h1>Portifólio</h1>
-        <div className={styles.markdownContent}>
-          <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-            {content}
-          </ReactMarkdown>
+      <section>
+        <div>
+          <MarkdownRenderer markdownContent={content} />
         </div>
       </section>
     </>
--- src/components/GitHubProfile/ProfileNav/index.jsx
+++ src/components/GitHubProfile/ProfileNav/index.jsx
@@ -4,35 +4,36 @@ import ProjectsIcon from "src/components/icon/svg/projetos.svg";
 import ArticlesIcon from "src/components/icon/svg/article.svg";
 import LinksIcon from "src/components/icon/svg/links.svg";
 import PortifolioIcon from "src/components/icon/svg/portifolio.svg";
+import Link from "next/link";
 
 function ProfileNav() {
   {
     return (
       <div className={styles.underlineNav}>
         <nav className={styles.underlineNavBody}>
-          <a
+          <Link
             href="/"
-            className={`${styles.underlineNavItem} ${styles.selected}`}
+            className={`${styles.underlineNavItem}`}
           >
             <HomeIcon />
             Home
-          </a>
-          <a href="#" className={styles.underlineNavItem}>
+          </Link>
+          <Link href="#" className={styles.underlineNavItem}>
             <ProjectsIcon />
             Projetos
-          </a>
-          <a href="#" className={styles.underlineNavItem}>
+          </Link>
+          <Link href="#" className={styles.underlineNavItem}>
             <ArticlesIcon />
             Artigos
-          </a>
-          <a href="/portifolio" className={styles.underlineNavItem}>
+          </Link>
+          <Link href="/portifolio" className={styles.underlineNavItem}>
             <PortifolioIcon />
             Portifolio
-          </a>
-          <a href="#" className={styles.underlineNavItem}>
+          </Link>
+          <Link href="#" className={styles.underlineNavItem}>
             <LinksIcon />
             Links
-          </a>
+          </Link>
         </nav>
       </div>
     );
